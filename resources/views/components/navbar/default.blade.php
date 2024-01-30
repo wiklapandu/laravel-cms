b2ae098b45a68119d925c92e50222b85f22d61c0
wiklapandu <wiklapandu2503@gmail.com>
2024-01-31 02:00:31 +0700

    feat: make layout for dashboard

---
+++ resources/views/components/navbar/default.blade.php
@@ -0,0 +1,62 @@
+<div class="grid bg-white min-h-[10vh] sticky top-0 shadow-lg">
+    <div class="flex mx-auto w-11/12 items-center p-3">
+        <div class="w-1/4">
+            <x-form-search />
+        </div>
+        <div class="ml-auto relative">
+            <x-dropdown align="right" width="48">
+                <x-slot name="trigger">
+                    @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                        <button
+                            class="flex items-center text-sm border-2 border-transparent p-2 focus:outline-none focus:border-gray-300 transition">
+                            <img class="h-9 w-9 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
+                                alt="{{ Auth::user()->name }}" />
+                            <span class="ml-3 font-bold text-lg">{{ Auth::user()->name }}</span>
+                        </button>
+                    @else
+                        <span class="inline-flex rounded-md">
+                            <button type="button"
+                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                {{ Auth::user()->name }}
+
+                                <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                </svg>
+                            </button>
+                        </span>
+                    @endif
+                </x-slot>
+
+                <x-slot name="content">
+                    <!-- Account Management -->
+                    <div class="block px-4 py-2 text-xs text-gray-400">
+                        {{ __('Manage Account') }}
+                    </div>
+
+                    <x-dropdown-link href="{{ route('profile.show') }}">
+                        {{ __('Profile') }}
+                    </x-dropdown-link>
+
+                    @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                        <x-dropdown-link href="{{ route('api-tokens.index') }}">
+                            {{ __('API Tokens') }}
+                        </x-dropdown-link>
+                    @endif
+
+                    <div class="border-t border-gray-200"></div>
+
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}" x-data>
+                        @csrf
+
+                        <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                            {{ __('Log Out') }}
+                        </x-dropdown-link>
+                    </form>
+                </x-slot>
+            </x-dropdown>
+        </div>
+    </div>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,122 +24,20 @@
             {{-- @livewire('navigation-menu') --}}
 
             <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow hidden">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
             <div class="grid lg:grid-cols-12 relative">
                 <div class="lg:col-span-2 lg:p-8 p-4 w-full bg-[#202A36] xl:block hidden shadow min-h-screen">
-                    <div class="brand mb-8">
-                        <a href="{{ route('dashboard') }}" class="inline-flex items-center text-white">
-                            <x-application-mark class="block h-9 w-auto" />
-                            <span class="ml-4 text-3xl font-medium">{{ env('APP_NAME') }}</span>
-                        </a>
-                    </div>
-                    <ul class="text-white grid gap-y-2">
-                        <li class="text-xl">
-                            <x-sidebar.nav-link href="{{ route('dashboard') }}">
-                                <x-slot name="icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-speedometer" viewBox="0 0 16 16">
-                                        <path d="M8 2a.5.5 0 0 1 .5.5V4a.5.5 0 0 1-1 0V2.5A.5.5 0 0 1 8 2M3.732 3.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707M2 8a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 8m9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5m.754-4.246a.39.39 0 0 0-.527-.02L7.547 7.31A.91.91 0 1 0 8.85 8.569l3.434-4.297a.39.39 0 0 0-.029-.518z"/>
-                                        <path fill-rule="evenodd" d="M6.664 15.889A8 8 0 1 1 9.336.11a8 8 0 0 1-2.672 15.78zm-4.665-4.283A11.95 11.95 0 0 1 8 10c2.186 0 4.236.585 6.001 1.606a7 7 0 1 0-12.002 0"/>
-                                    </svg>
-                                </x-slot>
-                                Dashboard
-                            </x-sidebar.nav-link>
-                        </li>
-                        <li class="text-xl">
-                            <x-sidebar.nav-link-dropdown>
-                                <x-slot name="icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-newspaper" viewBox="0 0 16 16">
-                                        <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5z"/>
-                                        <path d="M2 3h10v2H2zm0 3h4v3H2zm0 4h4v1H2zm0 2h4v1H2zm5-6h2v1H7zm3 0h2v1h-2zM7 8h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2z"/>
-                                    </svg>
-                                </x-slot>
-                                
-                                <x-slot name="trigger">
-                                    Blogs
-                                </x-slot>
-
-                                <ul class="grid ml-auto bg-slate-600 rounded-lg">
-                                    <li>
-                                         <x-sidebar.nav-link class="hover:bg-white hover:text-slate-600 rounded-none first:rounded-t-lg">
-                                            All Blogs
-                                        </x-sidebar.nav-link>
-                                    </li>
-                                    <li>
-                                         <x-sidebar.nav-link class="hover:bg-white hover:text-slate-600 rounded-none last:rounded-b-lg">
-                                            Add New Blogs
-                                        </x-sidebar.nav-link>
-                                    </li>
-                                </ul>
-                            </x-sidebar.nav-link-dropdown>
-                        </li>
-                    </ul>
+                    <x-sidebar.default />
                 </div>
                 <!-- Page Content -->
                 <main class="col-span-10 w-full">
-                    <div class="grid bg-white min-h-[10vh] sticky top-0 shadow-lg">
-                        <div class="flex mx-auto w-11/12 items-center p-3">
-                            <div class="w-1/4">
-                                <x-form-search/>
+                    <x-navbar.default />
+                    @if (isset($header))
+                        <header class="bg-white shadow hidden">
+                            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                                {{ $header }}
                             </div>
-                            <div class="ml-auto relative">
-                                <x-dropdown align="right" width="48">
-                                    <x-slot name="trigger">
-                                        @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                            <button class="flex items-center text-sm border-2 border-transparent p-2 focus:outline-none focus:border-gray-300 transition">
-                                                <img class="h-9 w-9 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                                                <span class="ml-3 font-bold text-lg">{{ Auth::user()->name }}</span>
-                                            </button>
-                                        @else
-                                            <span class="inline-flex rounded-md">
-                                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                                    {{ Auth::user()->name }}
-
-                                                    <svg class="ms-2 -me-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                                    </svg>
-                                                </button>
-                                            </span>
-                                        @endif
-                                    </x-slot>
-
-                                    <x-slot name="content">
-                                        <!-- Account Management -->
-                                        <div class="block px-4 py-2 text-xs text-gray-400">
-                                            {{ __('Manage Account') }}
-                                        </div>
-
-                                        <x-dropdown-link href="{{ route('profile.show') }}">
-                                            {{ __('Profile') }}
-                                        </x-dropdown-link>
-
-                                        @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                                            <x-dropdown-link href="{{ route('api-tokens.index') }}">
-                                                {{ __('API Tokens') }}
-                                            </x-dropdown-link>
-                                        @endif
-
-                                        <div class="border-t border-gray-200"></div>
-
-                                        <!-- Authentication -->
-                                        <form method="POST" action="{{ route('logout') }}" x-data>
-                                            @csrf
-
-                                            <x-dropdown-link href="{{ route('logout') }}"
-                                                    @click.prevent="$root.submit();">
-                                                {{ __('Log Out') }}
-                                            </x-dropdown-link>
-                                        </form>
-                                    </x-slot>
-                                </x-dropdown>
-                            </div>
-                        </div>
-                    </div>
+                        </header>
+                    @endif
                     {{ $slot }}
                 </main>
             </div>
